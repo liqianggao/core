72b7e7371b9cbfa7df294cb390aaf0ba5945ad4a
Michael Jaumann <meta_dev@yahoo.com>
2014-12-01 12:28:47 +0000

    make copy constructor private
    Change-Id: Ic6a36ecfbf24652d9e0a998d35ce1bfe3920eb6e

--- include/vcl/opengl/OpenGLContext.hxx
+++ include/vcl/opengl/OpenGLContext.hxx
@@ -149,6 +149,9 @@ struct GLWindow
 
 class VCLOPENGL_DLLPUBLIC OpenGLContext
 {
+private:
+    OpenGLContext(const OpenGLContext&);
+    OpenGLContext& operator=(const OpenGLContext&);
 public:
     OpenGLContext();
     ~OpenGLContext();

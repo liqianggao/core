d00bcda34c4b20ce642f56c6a20d1ef28efc9c8b
Daniel Sikeler <d.sikeler94@gmail.com>
2014-12-17 12:55:16 +0000

    CppUnitTest for tokenhandler in xmloff
    Change-Id: I3d9287b8e7cba0c123627b691a10cacf40bc2d2b

--- xmloff/Module_xmloff.mk
+++ xmloff/Module_xmloff.mk
@@ -29,6 +29,7 @@ $(eval $(call gb_Module_add_targets,xmloff,\
 $(eval $(call gb_Module_add_check_targets,xmloff,\
 	$(if $(and $(filter $(COM),MSC),$(MERGELIBS)),, \
 		CppunitTest_xmloff_uxmloff) \
+	CppunitTest_xmloff_tokenmap \
 ))
 
 $(eval $(call gb_Module_add_subsequentcheck_targets,xmloff,\
